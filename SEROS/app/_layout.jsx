dda091a859515d6b12b2bd9c7b76ffffee0ef442
Rehan Khan <203923326+IRehanKhanI@users.noreply.github.com>
2026-04-29 16:25:49 +0530

    first setup

---
+++ SEROS/app/_layout.jsx
@@ -0,0 +1,45 @@
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function RootLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Ashvek",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="marshal"
+        options={{
+          title: "Marshal",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="settings" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="rehan"
+        options={{
+          title: "Rehan",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="rylan"
+        options={{
+          title: "Rylan",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="star" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
