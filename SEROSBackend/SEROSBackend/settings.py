89be7dda30331668d98afd74e5f58bb282e63db6
Rehan Khan <203923326+IRehanKhanI@users.noreply.github.com>
2026-04-29 17:26:33 +0530

    db added

--- SEROSBackend/SEROSBackend/settings.py
+++ SEROSBackend/SEROSBackend/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,11 +77,13 @@ WSGI_APPLICATION = 'SEROSBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Replace the default SQLite database setting with this:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
